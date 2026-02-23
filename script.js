41cb1a3efbac91d8a1fb2a6c6a5ea270d5eb3d6d
salmanibneyrahman <darazbks@gmail.com>
2026-02-23 14:45:37 +0600

    script folder created

---
+++ script.js
@@ -0,0 +1,18 @@
+let interviewList = [];
+let rejectList = []
+let currentStatus = 'all'
+
+let total = document.getElementById('total');
+let interviewCount = document.getElementById('interviewCount')
+let rejectedCount = document.getElementById('rejectedCount');
+
+const allFilterBtn = document.getElementById('all-filter-btn')
+const thrivingFilterBtn = document.getElementById('interview-filter-btn')
+const strugglingFilterBtn = document.getElementById('rejected-filter-btn')
+
+const allCardSection = document.getElementById('allCards');
+const mainContainer = document.querySelector('main')
+const filterSection = document.getElementById('filtered-section')
+
+
+
